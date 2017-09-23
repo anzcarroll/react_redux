a07f145ce2689a00c7b143ce5dda980a1e568509
anzcarroll <anzcarroll@gmail.com>
2017-09-23 19:40:07 -0400

    getting sleepy, logging the 404 error from youtube api key...need to fix first thing when get back to it

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,25 @@
-
 import React, { Component } from 'react';
 
+
+
+
 class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            term: ''
+        }
+    }
+    _onInputChange = (e) => {
+        this.setState({ term : e.target.value })
+    }
     render() {
         return (
             <div>
-                <input/>
+                <input 
+                value={this.state.term}
+                onChange={this._onInputChange} 
+                />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import YOUTUBE_KEY from './../.env';
+import YTSearch from 'youtube-api-search';
 
-
+YTSearch({key: YOUTUBE_KEY, term: 'food'}, function(data){
+console.log(data);
+})
 const App = () => {
     return <div>
         <div>HELLLLO</div>
